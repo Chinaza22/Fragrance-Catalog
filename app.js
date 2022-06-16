83ed40acd5bd811669762e1318cddd265312904d
Chinaza Obidike <Obidikechinaza01@gmail.com>
2022-06-16 22:23:22 +0100

    initial

---
+++ app.js
@@ -0,0 +1,299 @@
+const menu = [
+    {
+      id: 1,
+      title: "A Hayati",
+      category: "Perfume",
+      price: 15000,
+      img: "imagez/ahayati.jpeg",
+      desc: ` A Hayati by Al Haramain Perfumes is an Amber Floral fragrance for women and men. The nose behind this fragrance is Christian Carbonnel. Top notes are Musk and Amber; the middle notes are Musk, Rose, and Sugar; base notes are Musk, Woody Notes and Agarwood (Oud). Product Name. HAYATI.`
+    },
+    {
+      id: 2,
+      title: "Ajwad",
+      category: "Perfume",
+      price: 9000,
+      img: "/imagez/Ajwad.jpeg",
+      desc: `   Perfume Ajwad Eau de Parfum by Lattafa Perfumes is a sweet, fruity-floral fragrance for women.
+      Fruity and light, the top note opens with lychee, bergamot and fruity notes.
+      Jasmine, roses and cinnamon fill the floral and warm heart notes. `,
+    },
+    {
+      id: 3,
+      title: "Amber & leather",
+      category: "Perfume",
+      price: 10000,
+      img: "Imagez/Amber.jpeg",
+      desc: `  Amber & Leather
+
+      Evoking the heat of the desert landscape of the west, the fragrance reveals itself like layers of sand blown in the wind. The combination of leather with warm amber, floral and spicy notes, produce an irresistible and dark fragrance.
+      
+      The matt black cylinder is cool and edgy in its appearance. It is the perfect perfume for men in its scent and appearance.`,
+    },
+    {
+      id: 4,
+      title: "Amberley",
+      category: "Perfume",
+      price: 16500,
+      img: "/Imagez/amberley.jpeg",
+      desc: `Housed in a very beautiful 100ml bottle by Alhambra with a golden cap. A wonderful choice as a gift or for your own everyday use inspired by Marfa from Memo Paris. The irresistible Oriental EDP blends energizing notes of Woody and Aromatic notes to create a bold and seductive fragrance. Top Notes: Oriental Notes Heart Notes: Woody Base Notes: Aromatic `,
+    },
+    {
+      id: 5,
+      title: "Amwaaj",
+      category: "Perfume",
+      price: 10000,
+      img: "/Imagez/Amwaaj.jpeg",
+      desc: `Amwaaj Interwood is irresistibly appealing, fascinating. It's awesomely amazing. With it on you, you are good to go,as you will be discussed after that ocassion. `,
+    },
+    {
+      id: 6,
+      title: "Aromatic Rouge",
+      category: "Perfume",
+      price: 12000,
+      img: "/Imagez/AromaticR.jpeg",
+      desc: `Alhambra aromatic Rouge EDP 100 ml (similar to Armand Basi in red)
+
+      Pyramid composition bergamot, ginger, cardamom, Mandarin black currant, leaf violets, peony, Rose, pink pepper (red berries), Jasmine, Lily Amber, Woody chord, oak moss, Musk, praline, vanilla group citrus fresh floral seductive and innocent, sensual and emotional, floral aroma of summer, Created for a charming seductive woman with a magical appeal, free and bold, naughty and gentle.`,
+    },
+    {
+      id: 7,
+      title: "Oud Mood",
+      category: "Perfume",
+      price: 8500,
+      img: "/Imagez/Oud.jpeg",
+      desc: `Oud Mood by Lattafa Perfumes is a fragrance for women and men. Top notes are Rose, Saffron and Pimento; middle notes are Agarwood (Oud), Caramel, Floral Notes and Patchouli; base notes are resins, Amber, Woody Notes, Incense and Musk.`,
+    },
+    {
+      id: 8,
+      title: "Soleil Rose",
+      category: "Perfume",
+      price: 12000,
+      img: "/Imagez/Soleil.jpeg",
+      desc: `FA Paris Soleil Rose EDP 100ml For Women, an enchanting composition. This is a love at first smell. `,
+    },
+    {
+      id: 9,
+      title: "Versencia noir",
+      category: "Perfume",
+      price: 8500,
+      img: "/Imagez/versencia.jpeg",
+      desc: `Versencia Noir is a magical, sensual, yet transparent scent. Absolutely feminine, delicate and very durable, reminiscent of the train of an evening dress..`,
+    },
+    {
+      id: 10,
+      title: "Oud Diffuser",
+      category: "Diffusers",
+      price: 10000,
+      img: "/Imagez/dif.jpeg",
+      desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    },
+    {
+        id: 11,
+        title: "Orchid bloom",
+        category: "Diffusers",
+        price: 8500,
+        img: "/Imagez/diff.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+      },
+      {
+        id: 12,
+        title: "Nature's Breath",
+        category: "Diffusers",
+        price: 9000,
+        img: "/Imagez/diffuser.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+      },
+      {
+        id: 13,
+        title: "Coral reef",
+        category: "Diffusers",
+        price: 10000,
+        img: "/Imagez/diffu.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+      },
+      {
+        id: 14,
+        title: "Arabian musk",
+        category: "Diffusers",
+        price: 15000,
+        img: "/Imagez/diffus.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+      },
+      
+      {
+        id: 15,
+        title: "Sycamore",
+        category: "Diffusers",
+        price: 7000,
+        img: "/Imagez/diffuse.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+      },
+      {
+        id: 16,
+        title: "Basil",
+        category: "Essential Oils",
+        price: 2500,
+        img: "/Imagez/basil.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+      },
+      {
+        id: 17,
+        title: "Cedarwood",
+        category: "Essential Oils",
+        price: 2500,
+        img: "/Imagez/cedarwood.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+      },
+
+      {
+        id: 18,
+        title: "Chamomile",
+        category: "Essential Oils",
+        price: 2500,
+        img: "/Imagez/chamomile.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+      },
+      {
+        id: 19,
+        title: "Fennel",
+        category: "Essential Oils",
+        price: 2500,
+        img: "/Imagez/fennel.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+      },
+
+      {
+        id: 20,
+        title: "Grapefruit",
+        category: "Essential Oils",
+        price: 2500,
+        img: "/Imagez/Grapefruit.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+      },
+
+      {
+        id: 21,
+        title: "Juniper",
+        category: "Essential Oils",
+        price: 2500,
+        img: "/Imagez/juniper.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+      },
+
+      {
+        id: 22,
+        title: "Indian Mural",
+        category: "Scented Candles",
+        price: 4500,
+        img: "/Imagez/boringC.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+      },
+
+      {
+        id: 23,
+        title: "White rose",
+        category: "Scented Candles",
+        price: 6500,
+        img: "/Imagez/flowe.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+      },
+
+      {
+        id: 24,
+        title: "Rosy feel",
+        category: "Scented Candles",
+        price: 8000,
+        img: "/Imagez/rose.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+      },
+
+      {
+        id: 25,
+        title: "Hibiscus breeze",
+        category: "Scented Candles",
+        price: 4500,
+        img: "/Imagez/natureC.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+      },
+
+      {
+        id: 26,
+        title: "Lover's embrace",
+        category: "Scented Candles",
+        price: 8500,
+        img: "/Imagez/loveC.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+      },
+
+      {
+        id: 27,
+        title: "Pink fall",
+        category: "Scented Candles",
+        price: 9500,
+        img: "/Imagez/pinkC.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+      },
+
+      {
+        id: 28,
+        title: "Dark Knight",
+        category: "Scented Candles",
+        price: 4800,
+        img: "/Imagez/bottle.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+      },
+
+
+  ];
+
+  const sectionMid= document.querySelector('.section-center');
+
+  const filterButtons = document.querySelectorAll('.filter-button');
+
+  //LOAD MENU
+  window.addEventListener('DOMContentLoaded', function(){
+    displayMenuItems(menu);
+  });
+
+  //FILTER MENU
+  filterButtons.forEach(function(button){
+    button.addEventListener('click', function(e){
+       const category = e.currentTarget.dataset.id;
+       
+       const fragranceCategory = menu.filter(function(menuItem){
+            if (menuItem.category===category){
+              return menuItem;
+            }
+       });
+
+       if(category=== 'all'){
+        displayMenuItems(menu);
+       }
+       else {
+         displayMenuItems(fragranceCategory);
+       }
+    });
+  });
+
+  function displayMenuItems(menuItems){
+    let displayMenu = menuItems.map(function(item){
+  //ADDING HTML CONTENT TO THE JScript
+      return `<article class="menu-item">
+      <img src=${item.img} alt=${item.title} class="photo" />
+      <div class="item-info">
+        <header>
+          <h4>${item.title}</h4>
+          <h4 class="price">#${item.price}</h4>
+        </header>
+        <p class="item-text">
+            ${item.desc}
+        </p>
+      </div>
+    </article>
+
+    `;
+  });
+  //DISPLAY CONTENT OF ARRAY AND INNER HTML TOGETHER
+  displayMenu = displayMenu.join("");
+ sectionMid.innerHTML = displayMenu;
+  };
